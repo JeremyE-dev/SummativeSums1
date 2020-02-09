74186923e98406f72fa73d487ef3bb3c8794a710
Jeremy Eudaly <eudaly_19@hotmail.comeugit>
2020-02-09 10:01:53 -0600

    completed assignment

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SummativeSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
+            int[] arr2 = { 999, -60, -77, 14, 160, 301 };
+            int[] arr3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
+                           140, 150, 160, 170, 180, 190, 200, -99 };
+
+            Console.WriteLine(sumOfArray(arr1));
+            Console.WriteLine(sumOfArray(arr2));
+            Console.WriteLine(sumOfArray(arr3));
+
+        }
+
+        public static int sumOfArray( int[] array)
+        {
+            int sum = 0;
+            foreach(int x in array)
+            {
+                sum += x;
+            }
+
+            return sum;
+        }
+    }
+}
